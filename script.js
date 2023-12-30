cdb0342f8f44e5320f602fa31bcfb26f4ff2ccb2
Seemran <swainseemran@gmail.com>
2023-12-30 23:18:15 +0530

    added some features

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ leftbtn1.addEventListener('click',function(event){
     conent.scrollLeft -= 1100;
     event.preventDefault();
 });
+
+const backtop = document.querySelector(".backtop");
+
+backtop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const sidebar=document.querySelector(".sidebar");
+const cross=document.querySelector(".fa-xmark");
+const black=document.querySelector(".black");
+const sidebtn=document.querySelector(".second-1");
+
+sidebtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+})
+
+cross.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+})
